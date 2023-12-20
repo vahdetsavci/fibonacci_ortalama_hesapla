cd09dc134fdad3c6bb724b3827a054ee49867c69
Vahdet Savci <savcivahdet@gmail.com>
2023-12-20 15:42:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace fibonacci_ortalama_hesapla;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Ortalama Hesaplama
+Bu uygulama kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların ortalamasını alıp ekrana yazdırır.
+
+# LICENSE
+[MIT](LICENSE)
+
